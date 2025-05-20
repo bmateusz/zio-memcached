4da94e40bc73f30e93d26d94a889f8dde1be85a9
Mate Borcsok <mate.borcsok@prezi.com>
2025-05-20 15:27:56 +0200

    Fix executor not retrying on receive errors

--- memcached/src/main/scala/zio/memcached/MemcachedExecutor.scala
+++ memcached/src/main/scala/zio/memcached/MemcachedExecutor.scala
@@ -178,7 +178,6 @@ object MemcachedExecutor {
         .via(RespValue.decoder)
         .collectSome
         .foreach(response => resQueue.take.flatMap(_.succeed(response)))
-        .orDie
 
     /**
      * Opens a connection to the server and launches send and receive operations. All failures are retried by opening a
